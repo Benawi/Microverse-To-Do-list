99e628a3401f0ba125c21f6385d939a0386f0de4
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-05-09 20:36:39 +0300

    Add array of data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './styles.css';
+const TASKS_DATA = [
+    {
+      index: 0,
+      description: 'Attend morning session',
+      completed: false,
+    },
+    {
+      index: 1,
+      description: 'write  code',
+      completed: true,
+    },
+    {
+      index: 2,
+      description: 'Solve hackerank challenges',
+      completed: false,
+    },
+    {
+      index: 3,
+      description: 'Meditate',
+      completed: false,
+    },
+  ];
