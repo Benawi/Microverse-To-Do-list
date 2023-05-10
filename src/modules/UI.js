b7e481182c20539d9d44752504c72b2b18d0b2f2
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-05-10 16:57:54 +0300

    Create UI class and it's constructor

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -3,3 +3,10 @@ import SyncIcon from '../assets/icons/sync-alt-solid.svg';
 import ArrowIcon from '../assets/icons/arrow-right-solid.svg';
 import EllipsisIcon from '../assets/icons/ellipsis-v-solid.svg';
 
+export default class UI {
+  constructor(store) {
+   
+    this.store = store;
+  }
+
+}
